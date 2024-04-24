efdec10282f7692540982ae719b23efc6b17ef7c
hirakinii <hiraki@nii.ac.jp>
2024-04-24 14:37:28 +0900

    update main.py to generate a csv file

--- src/main.py
+++ src/main.py
@@ -3,13 +3,23 @@
 
 import numpy as np
 import os
+import pandas as pd
 
 
 def main():
+    # test1: random int with .dat
     shape = (1024, 1024)
     data = np.random.randint(0, 10, shape, dtype=int)
-    dstpath = os.path.join(os.path.dirname(__file__), "../output/test.dat")
+    dstpath = os.path.join(os.path.dirname(__file__), f"../output/test.dat")
     np.savetxt(dstpath, data, delimiter=",", fmt="%d")
+
+    # test2: random int with .csv and header
+    shape = (1024*256, 3)
+    data = np.random.randint(0, 10, shape, dtype=int)
+    columns = ["x", "y", "z"]
+    df = pd.DataFrame(data, columns=columns)
+    dstpath = os.path.join(os.path.dirname(__file__), f"../output/test.csv")
+    df.to_csv(dstpath, index=None)
 
 
 if __name__ == "__main__":

d71e01526fddce0bd0ae5c4594e627ceadfb0518
hirakinii <hiraki@nii.ac.jp>
2024-04-24 14:18:46 +0900

    update main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,16 @@
 """main.py
 """
 
+import numpy as np
+import os
+
+
+def main():
+    shape = (1024, 1024)
+    data = np.random.randint(0, 10, shape, dtype=int)
+    dstpath = os.path.join(os.path.dirname(__file__), "../output/test.dat")
+    np.savetxt(dstpath, data, delimiter=",", fmt="%d")
+
 
 if __name__ == "__main__":
-    print("hello world!")
+    main()
